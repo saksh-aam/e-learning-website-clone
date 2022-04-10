f4f5b6e21d2225b08667b07d7210a088509528e5
Saksham <saksham.kathuria0211@gmail.com>
2022-04-11 02:11:45 +0530

    .

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
   <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav ml-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="#">Login</a>
+        <a className="nav-link" href="#">Logout</a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#">Courses</a>
